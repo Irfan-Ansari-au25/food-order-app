ed7adf943b1a6901838c72502a1f0b880f2676dd
Irfan-Ansari-au25 <irfan398649@gmail.com>
2022-01-19 23:42:28 +0530

    Created modal using portal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Fragment } from "react";
+import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 
 const App = () => {
   return (
     <Fragment>
+      <Cart></Cart>
       <Header />
       <main>
         <Meals />
